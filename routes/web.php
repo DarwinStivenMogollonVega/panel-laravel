60b389935db0bba5b0c33620361d0d2c6f8a4afd
JCMR05 <juancamilomorarangel@gmail.com>
2025-10-17 17:27:17 -0500

    git pull origin main --rebase hicimos el recordar contraseña y el formulario de reseteo de contraseña con su verificacion
    git pull origin main --rebasegit pull origin main --rebase
    
    git pull origin main --rebase

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,14 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RoleController;
+<<<<<<< HEAD
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+=======
+use App\Http\Controllers\Auth\PerfilController;
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Auth\ResetPasswordController;
+>>>>>>> 99b4c79 (hicimos el recordar contraseña y el formulario de reseteo de contraseña con su verificacion)
 
 Route::get('/', function () {
     return view('welcome');
@@ -45,6 +52,7 @@ Route::post('logout', function(){
 Route::resource('roles', RoleController::class);
 
 
+<<<<<<< HEAD
 Route::middleware(['guest'])->group(function(){
 
 Route::get('login', function(){
@@ -54,3 +62,13 @@ Route::post('login', [AuthController::class, 'login'])->name('login.post');
 Route::get('/registro', [RegisterController::class, 'showRegisterForm'])->name('registro');
 Route::post('/registro', [RegisterController::class, 'registration'])->name('registro.store');
 });
+=======
+    Route::get('/registro', [RegisterController::class, 'showRegistrationForm'])->name('registro');
+    Route::post('/registro', [RegisterController::class, 'registration'])->name('registro.store');
+
+    Route::get('password/reset', [ResetPasswordController::class, 'showRequestForm'])->name('password.request');
+    Route::post('password/email', [ResetPasswordController::class, 'sendResetLinkEmail'])->name('password.send-link');
+    Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::post('password/reset', [ResetPasswordController::class, 'resetPassword'])->name('password.update');
+});
+>>>>>>> 99b4c79 (hicimos el recordar contraseña y el formulario de reseteo de contraseña con su verificacion)
